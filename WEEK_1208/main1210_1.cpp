a14eb21b94825009a0250c5fd79329919ca0d8ef
Thispring <oh42gs@gmail.com>
2025-12-11 10:01:13 +0900

    update_1210

---
+++ WEEK_1208/main1210_1.cpp
@@ -0,0 +1,58 @@
+// CPP 함수
+#include <iostream>
+
+// 함수
+// 코드를 모듈화 하고 재활용하기 위함
+// 새로운 기능을 만들때 편리
+
+// 함수는 [반환타입 함수이름 (매개변수) {함수 기능}]으로 이루어져 있음 
+int Add(int a, int b) { return a + b; }
+
+// (*)실제 컴퓨터 스택에서 함수가 어떻게 동작하는지에 대해 생각하기
+// 스택은 선입후출(후입선출)의 자료구조
+
+int Pow(int Num, int Count);
+
+int recPow(int Num, int Count)
+{
+	if (Count == 0)
+		return 1;
+	
+	return Num * recPow(Num, Count - 1);
+}
+
+// main도 하나의 함수로 컴파일러의 진입 함수
+int main()
+{
+	int c = Add(100, 200);
+	
+	// [*Lab_0]
+	// 제곱근 구하는 함수
+	// 함수구조: int Pow(int Num, int Count)
+	// 사용방법: int Num = Pow(2, 10);
+	// Num에 1024 저장
+	int res = Pow(2, 10);
+	printf("%d\n", res);
+
+	printf("%d\n", recPow(2, 10));
+
+	return 0;
+}
+// 지역변수와 전역변수
+
+
+// F10	: 프로시저 단위로 실행
+// F11	: 최소 작업단위로 코드 실행
+
+int Pow(int Num, int Count)
+{
+	int res;
+	for (res = 1; 1 <= Count; --Count)
+	{
+		res *= Num;
+	}
+	return res;
+
+	//if (Count > 0)
+		//return Pow(Num, Count - 1);
+}
---
+++ WEEK_1208/main1210_2.cpp
@@ -0,0 +1,96 @@
+// CPP 함수 문법
+#include <iostream>
+#include <ctime>
+
+// Default 인자
+// 기본으로 보장되는 입력값을 미리 설정
+// Default 인자는 뒤에서 부터 세팅이 가능
+
+void funC(int a, int b, int c = 0)
+{
+	std::cout << a + b + c << std::endl;
+}
+
+// (*)함수 오버로딩
+// 함수의 이름이 같더라도
+// 매개변수의 개수나 타입이 다를 경우
+// 같은 이름의 함수 중복 가능
+void funC(int a)
+{
+	std::cout << a << std::endl;
+}
+// (*)함수 오버로딩과 오버라이딩의 차이
+// https://brunch.co.kr/@kimkm4726/2
+
+// [*Lab_0]
+// 버블정렬
+// 작은 수 부터 정렬
+void BubbleSort(int(&Arr)[10], bool isAscending)
+{
+	int tmpA[10] = { };
+	int tmp = 0;
+	int tmpIndex = 0;
+	int index;
+	int assenNum;
+
+	if (isAscending)
+	{
+		index = 9;
+		assenNum = -1;
+	}
+	else
+	{
+		index = 0;
+		assenNum = 1;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		tmpA[i] = Arr[i];
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			// 제일 큰 수를 기준으로 배열 순회 후
+			// Arr의 마지막 인덱스부터 채우기
+			if (tmpA[j] >= tmp)
+			{
+				tmp = tmpA[j];
+				tmpIndex = j;
+			}
+		}
+
+		Arr[index] = tmp;
+		tmpA[tmpIndex] = 0;
+		index += assenNum;
+		tmp = 0;
+		tmpIndex = 0;
+	}
+}
+
+int main()
+{
+	funC(1, 2);
+	funC(200);
+
+	// 무작위로 수를 집어넣고, 작은 수 부터 정렬
+	srand(time(NULL));
+	
+	int Arr[10];
+	for (int i = 0; i < 10; i++)
+		Arr[i] = rand()%100 + 1;
+	
+	std::cout << "Before Sort" << std::endl;
+	for (int i = 0; i < 10; i++)
+		std::cout << Arr[i] << " ";
+	BubbleSort(Arr, true);
+	std::cout << std::endl << "After Sort" << std::endl;
+	for (int i = 0; i < 10; i++)
+		std::cout << Arr[i] << " ";
+
+
+
+	return 0;
+}
